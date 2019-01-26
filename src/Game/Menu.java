89fc079645d9399708007a51d71646469f90a878
Ryland O'Quinn <rlmoq9@gmail.com>
2019-01-25 22:17:55 -0700

    working on creating a character

---
+++ src/Game/Menu.java
@@ -0,0 +1,20 @@
+package Game;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static int menu(String[] input) {
+        Scanner scan = new Scanner(System.in);
+        int length = input.length;
+        int ans = -1;
+        for (int x = 0; x < length; x++) {
+            System.out.println((x + 1) + ". " + input[x]);
+        }
+        while (ans <= 0 || ans > length)  {
+            System.out.println("Please enter a number between 1 and " + length);
+            ans = scan.nextInt();
+            scan.nextLine();
+        }
+        return ans;
+    }
+}
--- src/Game/createCharacter.java
+++ src/Game/createCharacter.java
@@ -1,34 +1,22 @@
 package Game;
 
-import java.util.Scanner;
 
 public class createCharacter {
     public  createCharacter(){
-        System.out.println("Hello and welcome to (insert game name). This is a spin off of Dungeons and dragons focused on combat with AI enemies");
+        Menu menu = new Menu();
+        String[] classes = {"barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorcerer", "wizard"};
+        String[] races = {"human", "dwarf", "elf", "gnome", "half-lings", "half-elf", "half-orc"};
+        System.out.println("Hello and welcome to (insert game name). This is a spin off of Dungeons and dragons focused on combat with basic AI enemies");
         System.out.println();
         System.out.println("To begin please roll the dice to determine your ability scores" ); //TODO tell player what ability scores do
+        System.out.println(menu.menu(classes));
+    }
 
+    public int[] abilityRoll(){
+        Dice die = new Dice();
+        int[] roll = new int[3];
+        for (int i = 0; i < 5; i++)
     }
-    // barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, and wizard.
-    public static String menu() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("barbarian");
-        System.out.println("bard");
-        System.out.println("cleric");
-        System.out.println("druid");
-        System.out.println("fighter");
-        System.out.println("monk");
-        System.out.println("paladin");
-        System.out.println("ranger");
-        System.out.println("rogue");
-        System.out.println("sorcerer");
-        System.out.println("wizard");
-        String ans = "z";
-        while (!ans.equals("a") && !ans.equals("b") && !ans.equals("c") && !ans.equals("d") && !ans.equals("e")) {
-            System.out.println("Please enter one letter a-e with out spaces and lowercase");
-            ans = scan.nextLine();
-        }
-        return ans;
-    }
+
 
 }
