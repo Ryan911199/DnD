1dc7bcc582e15da9cfe20d123339ceaeebaf00d3
Shaun <sdjone23@asu.edu>
2019-03-29 19:36:40 -0700

    Added armor
    added armor

---
+++ src/Items/Armor/ArmorTypes/boots/lightBoots.java
@@ -0,0 +1,10 @@
+package Items.Armor.ArmorTypes.boots;
+import Items.Armor.*;
+public class lightBoots extends boots
+{
+
+    @Override
+    public String toString() {
+        return "Light Boots";
+    }
+}
--- src/Items/Armor/ArmorTypes/chestplate/ArcaneChestplate.java
+++ src/Items/Armor/ArmorTypes/chestplate/ArcaneChestplate.java
@@ -4,7 +4,10 @@ public class ArcaneChestplate extends chestplate
 {
     public ArcaneChestplate()
     {
-
+        value = 5000;
+        weight = 15;
+        armorBonus = 15;
+        dexBonus = 7;
     }
     @Override
     public String toString() {
