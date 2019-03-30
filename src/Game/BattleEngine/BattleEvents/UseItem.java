504117de9bb863db59ef0aad1faaaab3ca205b67
Ryland O'Quinn <rlmoq9@gmail.com>
2019-03-29 19:36:08 -0700

    update to BattleEngine

--- src/Game/BattleEngine/BattleEvents/UseItem.java
+++ src/Game/BattleEngine/BattleEvents/UseItem.java
@@ -1,8 +1,14 @@
 package Game.BattleEngine.BattleEvents;
 
+import Items.*;
+
 public class UseItem extends BattleEvent {
+    private Consumable Item;
+    public UseItem(Consumable itmeInput){
+        Item = itmeInput;
+    }
     @Override
     public void doEvent() {
-
+        Owner.hitPoints = Owner.hitPoints + Item.consume();
     }
 }
