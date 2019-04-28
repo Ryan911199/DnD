2ba13c175dd39f6e558f25b332f7eb0f8431d1f8
Ryland <rlmoq9@gmail.com>
2019-04-27 18:35:48 -0700

    update

--- src/Game/BattleEngine/BattleEvents/Wait.java
+++ src/Game/BattleEngine/BattleEvents/Wait.java
@@ -3,6 +3,8 @@ package Game.BattleEngine.BattleEvents;
 public class Wait extends BattleEvent {
     private String reason = null;
 
+    public Wait() {
+    }
     public Wait(String Reason) {
         reason = Reason;
     }
